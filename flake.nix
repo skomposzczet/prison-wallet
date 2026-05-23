8c2ec8a999c0a790d0c1ebbf6a5fb2756729980d
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2026-05-23 09:57:47 +0200

    bruh

--- flake.nix
+++ flake.nix
@@ -1,38 +1,93 @@
 {
-  description = "Pytorch with cuda enabled";
+  description = "A Nix-flake-based Python development environment";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
   outputs =
-    { self, nixpkgs }:
+    { self, ... }@inputs:
 
     let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-      };
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+
+      /*
+        Change this value ({major}.{min}) to
+        update the Python virtual-environment
+        version. When you do this, make sure
+        to delete the `.venv` directory to
+        have the hook rebuild it for the new
+        version, since it won't overwrite an
+        existing one. After this, reload the
+        development shell to rebuild it.
+        You'll see a warning asking you to
+        do this when version mismatches are
+        present. For safety, removal should
+        be a manual step, even if trivial.
+      */
+      version = "3.13";
     in
     {
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        # UV_PYTHON = "${pkgs.python314}/bin/python";
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.stdenv.cc.cc
-          pkgs.zlib
-          # "/run/opengl-driver"
-        ];
-        packages = with pkgs; [
-          python313
-          python313Packages.venvShellHook
-          python313Packages.tkinter
-          uv
-          # ruff
-          # ty
-        ];
-        venvDir = ".venv";
-        postShellHook = ''
-          export UV_LINK_MODE=copy
-        '';
-      };
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        let
+          concatMajorMinor =
+            v:
+            pkgs.lib.pipe v [
+              pkgs.lib.versions.splitVersion
+              (pkgs.lib.sublist 0 2)
+              pkgs.lib.concatStrings
+            ];
+
+          python = pkgs."python${concatMajorMinor version}";
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            venvDir = ".venv";
+
+            postShellHook = ''
+              venvVersionWarn() {
+              	local venvVersion
+              	venvVersion="$("$venvDir/bin/python" -c 'import platform; print(platform.python_version())')"
+
+              	[[ "$venvVersion" == "${python.version}" ]] && return
+
+              	cat <<EOF
+              Warning: Python version mismatch: [$venvVersion (venv)] != [${python.version}]
+                       Delete '$venvDir' and reload to rebuild for version ${python.version}
+              EOF
+              }
+
+              venvVersionWarn
+            '';
+
+            packages = with python.pkgs; [
+              venvShellHook
+              pip
+              numpy
+              matplotlib
+              pkgs.gcc
+              pandas
+              plotly
+              scipy
+              base58
+              customtkinter
+              python-dotenv
+              pkgs.solc
+            ];
+          };
+        }
+      );
     };
 }
